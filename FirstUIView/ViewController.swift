895d49415844a31a904e4faf85c5ef580f815616
ciaraiseul <139107560+ciaraiseul@users.noreply.github.com>
2024-08-07 10:39:11 +0700

    update

--- FirstUIView/ViewController.swift
+++ FirstUIView/ViewController.swift
@@ -15,28 +15,6 @@ class ViewController: UIViewController {
            super.viewDidLoad()
        }
 
-       @IBAction func captureLeftView(_ sender: UITapGestureRecognizer) {
-           if let image = captureView(view: leftView) {
-               capturedImageView.image = image
-           }
-       }
-
-       @IBAction func captureRightView(_ sender: UITapGestureRecognizer) {
-           if let image = captureView(view: rightView) {
-               capturedImageView.image = image
-           }
-       }
-
-       func captureView(view: UIView) -> UIImage? {
-           UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, UIScreen.main.scale)
-           defer { UIGraphicsEndImageContext() }
-           if let context = UIGraphicsGetCurrentContext() {
-               view.layer.render(in: context)
-               let image = UIGraphicsGetImageFromCurrentImageContext()
-               return image
-           }
-           return nil
-       }
-
+      
 }
 
